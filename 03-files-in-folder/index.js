cb8c06890b404b0d59f3de4bd9f523689aa478a5
Tati-Zhurr <zhtv88@gmail.com>
2022-11-07 19:45:55 +0100

    fix: add function to clean "files-copy" before overwriting the files

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -4,10 +4,10 @@ const folderPath = path.join(__dirname, 'secret-folder');
 
 (async() =>{
     try {
-  const files = await fs.promises.readdir(folderPath, {withFileTypes: true});
-  for (const file of files){
-    let dataOfFile =[];
-    if (file.isFile()){
+      const files = await fs.promises.readdir(folderPath, {withFileTypes: true});
+      for (const file of files){
+      let dataOfFile =[];
+      if (file.isFile()){
         const fileName = path.parse(file.name).name;
         const fileExtent = path.extname(file.name).slice(1);
         dataOfFile.push(fileName);
@@ -18,7 +18,7 @@ const folderPath = path.join(__dirname, 'secret-folder');
             const sizeKB =stats.size;
             const fileSize = `${parseInt(stats.size/1024*1000)/1000}kB`;
             dataOfFile.push(fileSize);
-            const dataOfFileString = dataOfFile.join('-');
+            const dataOfFileString = dataOfFile.join(' - ');
             console.log(dataOfFileString);
           });
     }
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,6 +3,16 @@ const path = require('path');
 const pathFolderCopy = path.join(__dirname, 'files-copy');
 
 
+//delete folder
+/*(async ()=> {
+    fs.rm(pathFolderCopy, { recursive: true, force: true }, err =>{
+        if (err) {
+            throw err;
+        }
+    })
+    
+})();*/
+
 //create folder
 (async ()=> {
     fs.mkdir(pathFolderCopy, { recursive: true }, err =>{
@@ -12,27 +22,51 @@ const pathFolderCopy = path.join(__dirname, 'files-copy');
     }
 )})();
 
+//delete files in files-copy
+(async() =>{
+    try {
+        const filesCopiedBefore = await fs.promises.readdir(pathFolderCopy, {withFileTypes: true});
+        if (filesCopiedBefore.length){
+            for (const file of filesCopiedBefore){
+                if (file.isFile()){
+                  const fileCopyPath = path.join(__dirname, 'files-copy', file.name);
+                  fs.unlink(fileCopyPath, (err)=>{
+                                                if (err) {
+                                                  throw err;
+                                                }
+                                                });
+                }
+            }
+        }
+       
+        } catch (err) {
+          console.error(err);
+          }
+}) ();
+
+
+
+
 //read folder and copy files
 const folderPath = path.join(__dirname, 'files');
 (async() =>{
     try {
         const files = await fs.promises.readdir(folderPath, {withFileTypes: true});
         for (const file of files){
-        if (file.isFile()){
-        const filePath = path.join(__dirname, 'files', file.name);
-        const fileCopyPath = path.join(__dirname, 'files-copy', file.name);
-        fs.copyFile(filePath, fileCopyPath, (err)=>{
-            if (err) {
-                throw err;
-            }
-        });
-    }
-
-  }
-  console.log('All files from folder "files" were copied to the folder "files-copy"');
-} catch (err) {
-  console.error(err);
-}
+          if (file.isFile()){
+            const filePath = path.join(__dirname, 'files', file.name);
+            const fileCopyPath = path.join(__dirname, 'files-copy', file.name);
+            fs.copyFile(filePath, fileCopyPath, (err)=>{
+                                                       if (err) {
+                                                         throw err;
+                                                        }
+                                                       });
+          }
+        }
+          console.log('All files from folder "files" were copied to the folder "files-copy"');
+        } catch (err) {
+          console.error(err);
+          }
 }) ();
 
 
