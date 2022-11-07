127da6432f1f3e18fdc20db9a1169a9e569af2a8
Tati-Zhurr <zhtv88@gmail.com>
2022-11-07 20:05:13 +0100

    refactor: add message at the end

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -4,16 +4,12 @@ const pathStyles = path.join(__dirname, 'styles');
 const pathBundle = path.join (__dirname, 'project-dist', 'bundle.css');
 const output = fs.createWriteStream(pathBundle);
 
-
-
-
-
 //read files in Styles
 (async() =>{
     try {
         const files = await fs.promises.readdir(pathStyles, {withFileTypes: true});
         fs.promises.writeFile(pathBundle, '');
-       for (const file of files){
+        for (const file of files){
           const fileExtent = path.extname(file.name).slice(1);
           if (file.isFile()&& (fileExtent === 'css')){
             const fileSourcePath = path.join(__dirname, 'styles', file.name);
@@ -24,29 +20,9 @@ const output = fs.createWriteStream(pathBundle);
             input.on ('error', error => console.log('Error', error.message));
           }
         }
-
-       // output.write(styleText.join(''));
+        console.log('Styles are merged.');
         } catch (err) {
           console.error(err);
         }     
 }) ()
 
-function writeToBunddle(files){
-    let styleText=[];
-    for (const file of files){
-        const fileExtent = path.extname(file.name).slice(1);
-        if (file.isFile()&& (fileExtent === 'css')){
-          const fileSourcePath = path.join(__dirname, 'styles', file.name);
-          const input = fs.createReadStream(fileSourcePath);
-          input.on('data', (chunk) => {
-            console.log(styleText);
-              styleText.push(chunk);
-          });
-          input.on ('error', error => console.log('Error', error.message));
-        }
-      }
-      console.log(styleText);
-      output.write(styleText.join(''));
-}
-/*console.log(styleText);
-output.write(styleText);*/
